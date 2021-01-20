c37636f700747e538c3b753d857a92c8d9f19e0d
KSantosh24 <73108688+KSantosh24@users.noreply.github.com>
2021-01-20 13:53:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,47 @@
 //Create variables here
-
+var dog,dog2,dogimg,happydog;
+var canvas;
 function preload()
 {
   //load images here
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
+  dog = loadImage("dogimg.png",dogimg);
+  dog2 = loadImage("dogimg1.png",happydog);
+
+
+
 }
 
 function setup() {
-	createCanvas(500, 500);
-  
+  canvas = createCanvas(500,500);
+  database = firebase.database();
+  dog = createsprite(400,400,50,50);
+  dog.addImage(dogimg);
+  var foodstork  = database.ref('food');
+  foodstork.no("value",readstrok);
 }
 
 
-function draw() { 
-  background(46,139,87) 
-if(keyDown(UP_ARROW)){
-  writeStock(foodS);
-  dog.addImage(dogHappy);
-}
+function draw() {  
+  background(46, 139, 87);
   drawSprites();
   //add styles here
-function readStock(data){
-  foodS=data.val();
-}
-function writeStock(x){
-  Food:x
-}
+  if (keyWantDown(UP_ARROW)){
+     writeStrok(foodS);
+     dog.addImage(happydog);
+  }
 }
 
+function readstrok(data){
+foodS=data.val();
+}
 
-
+function writeStrok(x){
+  if (x<=0) {
+    x=0
+  } else {
+    x=x-1
+  }
+database.ref('/').updata({
+  food:x
+})
+}
